5608ebf781fad74796284f2e78a70cf0ea236e87
hidetaka hasegawa <>
2020-08-27 21:08:13 +0900

    controller動くようにした

---
+++ app/models/studio_booking.rb
@@ -0,0 +1,18 @@
+class StudioBooking < ApplicationModel
+  ENDPOINT = "https://www.noahstudio.jp/noahweb/Webs/search/1/0/1"
+end
+
+# await fetch("https://www.noahstudio.jp/noahweb/Webs/search/1/0/1", {
+#   "credentials": "include",
+#   "headers": {
+#       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
+#       "Accept": "application/json, text/javascript, */*; q=0.01",
+#       "Accept-Language": "ja,en-US;q=0.7,en;q=0.3",
+#       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+#       "X-Requested-With": "XMLHttpRequest"
+#   },
+#   "referrer": "https://www.noahstudio.jp/noahweb/webs/searchresults",
+#   "body": "all_branch_id=0&branchlist=78&car_flg=0&recorder_flg=0&sub_room_flg=0&piano_flg=0&sdate=2020-08-30+13%3A00%3A00&edate=2020-08-30+15%3A00%3A00&roomsizelist=6_10%2C10_20",
+#   "method": "POST",
+#   "mode": "cors"
+# });
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post "webhooks", to: 'webhooks#receive'
+  get "test", to: 'webhooks#test'
 end
