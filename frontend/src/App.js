63eb4c5aa4b5fe2b82179e65fa764724860864b7
hide <razyetts@hotmail.co.jp>
2020-09-05 15:13:39 +0900

    検索ボタン設置

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
 
 import { Layout } from 'antd';
+import { Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Footer, Content } = Layout;
+
+const title = "ノアスタジオかんたん検索(非公式)"
 
 function App() {
+  const [loading, setLoading] = useState(false)
+
+
+  function search() {
+    setLoading(true)
+  }
+
   return (
     <>
       <Layout>
-        <Header>Header</Header>
-        <Content>Content</Content>
+        <h1>{title}</h1>
+        <Content>
+          <div>
+            <Button type="primary" icon={<SearchOutlined />} size='large' loading={loading} onClick={search}>
+              スタジオ検索
+            </Button>
+          </div>
+        </Content>
         <Footer>Footer</Footer>
       </Layout>
     </>
